a6bdf684cbd168199157e6bf046345ce275aff04
pkisensee <pkisensee@msn.com>
2024-08-19 21:24:07 -0700

    Initial checkin. No functionality.

---
+++ Mp3TagReader.cpp
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Mp3TagReader.cpp
+//
+//  Copyright © Pete Isensee (PKIsensee@msn.com).
+//  All rights reserved worldwide.
+//
+//  Permission to copy, modify, reproduce or redistribute this source code is
+//  granted provided the above copyright notice is retained in the resulting 
+//  source code.
+// 
+//  This software is provided "as is" and without any express or implied
+//  warranties.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <filesystem>
+
+//using namespace PKIsensee;
+namespace fs = std::filesystem;
+
+int __cdecl main( int, char** )
+{
+}
